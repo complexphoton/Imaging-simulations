92e818de3a9072e6cec84b2168825fb6630bfc49
zywang <zywang36@gmail.com>
2023-08-17 02:31:12 -0400

    Upload recon_all.m

---
+++ scripts/image_reconstruction/recon_all.m
@@ -0,0 +1,9 @@
+data_dir = fullfile('.', 'data', 'large_system');
+%data_dir = fullfile('.', 'data', 'large_system_no_weak');
+tic
+recon_smt(data_dir); % reconstruct the SMT image
+recon_rcm(data_dir); % reconstruct the RCM image
+recon_ocm_or_oct('ocm', data_dir); % reconstruct the OCM image
+recon_ocm_or_oct('oct', data_dir); % reconstruct the OCT image
+recon_isam(data_dir); % reconstruct the ISAM image
+toc
